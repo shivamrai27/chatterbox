4198efef8dc3bd1d6bdebe5d3cbbf17f135f4270
shivamrai27 <200shivamrai@gmail.com>
2024-05-31 12:16:49 +0530

    socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("https://chatterbox-prod.onrender.com/", {
+			const socket = io("https://chatterbox-be5q.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
